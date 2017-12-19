a3f708c144228fe225d8ddefc66b2ec52936829f
Ben Webb <bjwebb67@googlemail.com>
2017-12-19 15:55:34 +0000

    Add pdf script
    from https://github.com/open-contracting/standard/issues/588#issuecomment-346345181

---
+++ make_pdf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for lang in en es fr; do
+wkhtmltox/bin/wkhtmltopdf --no-stop-slow-scripts --javascript-delay 10000 --disable-smart-shrinking --print-media-type toc http://standard.open-contracting.org/latest/$lang/{,getting_started/{,use_cases/,contracting_process/,building_blocks/,releases_and_records/,publication_patterns/,validation/},schema/{,reference/,release/,release_package/,records_reference/,record_package/,merging/,identifiers/,codelists/,conformance_and_extensions/,deprecation/,changelog/},implementation/{,levels/,licensing/,publication_policy/,serialization/,hosting/,registration/,related_processes/,amendments/},extensions/{,developing/,community/,bids/,enquiries/,location/,lots/,milestone_documents/,participation_fee/,party_details/,process_title/},support/{,tools/,history_and_development,governance/,credits/}} OCDS_$lang.pdf
+done
+
