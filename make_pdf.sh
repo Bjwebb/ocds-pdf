bbb2a9f36dc95ca6e3f237f694d8524b19c395c4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-07-08 13:41:39 -0400

    Allow shell to determine which wkhtmltopdf command to use

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 for lang in en es fr; do
-wkhtmltox/bin/wkhtmltopdf \
+wkhtmltopdf \
     --no-stop-slow-scripts --javascript-delay 10000 --disable-smart-shrinking --print-media-type \
     --header-html header.html \
     toc http://standard.open-contracting.org/latest/$lang/{,getting_started/{,use_cases/,contracting_process/,building_blocks/,releases_and_records/,publication_patterns/,validation/},schema/{,reference/,release/,release_package/,records_reference/,record_package/,merging/,identifiers/,codelists/,conformance_and_extensions/,deprecation/,changelog/},implementation/{,levels/,licensing/,publication_policy/,serialization/,hosting/,registration/,related_processes/,amendments/},extensions/{,developing/,community/,bids/,enquiries/,location/,lots/,milestone_documents/,participation_fee/,party_details/,process_title/},support/{,tools/,history_and_development,governance/,credits/}} OCDS_$lang.pdf
 done
-
